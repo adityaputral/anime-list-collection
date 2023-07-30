9157796064164490ce47b129f6d4c7615ba3c8d2
Aditya Leksono <adityaputra7997@gmail.com>
2023-07-31 05:18:53 +0700

    add dummy collection card

--- src/pages/collection-listing/CollectionListing.tsx
+++ src/pages/collection-listing/CollectionListing.tsx
@@ -1,4 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { CardActionArea } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import {
   addCollection,
   removeCollection,
@@ -8,6 +15,7 @@ import {
 } from '../../store/animeCollections';
 
 export default function Counter() {
+  const navigate = useNavigate();
   const collections = useSelector(
     (state) => state.animeCollections.collections
   );
@@ -15,6 +23,48 @@ export default function Counter() {
 
   return (
     <div>
+      <h2>Collection Listing</h2>
+      <Grid container spacing={3}>
+        {collections &&
+          collections.length > 0 &&
+          collections.map((collection, i: number) => {
+            let showMore = true;
+            function toggleShowMore() {
+              showMore = !showMore;
+            }
+            return (
+              <>
+                <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+                  <CardActionArea>
+                    <Card
+                      sx={{ height: '100%' }}
+                      onClick={() =>
+                        navigate(`collection-list/${collection.id}/detail`)
+                      }
+                    >
+                      <CardMedia
+                        sx={{ height: 140 }}
+                        image={
+                          collection.animeList &&
+                          collection.animeList.length > 0
+                            ? collection.animeList[0].bannerImage
+                            : ''
+                        }
+                        title={collection.name}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {collection.name}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </CardActionArea>
+                </Grid>
+              </>
+            );
+          })}
+      </Grid>
+
       <div>
         <button
           aria-label="Increment value"
