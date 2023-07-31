01a99fb9a75d5bd8b8909ebd8147ef3aa7fd0d1a
Aditya Leksono <adityaputra7997@gmail.com>
2023-07-31 07:05:22 +0700

    apply edit collection name

--- src/pages/collection-detail/CollectionDetail.tsx
+++ src/pages/collection-detail/CollectionDetail.tsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import { editCollection, removeAnime } from '../../store/animeCollections';
 import Card from '../../components/Card/Card';
 
 function CollectionDetail() {
   const [collectionDetailData, setCollectionDetailData] = useState({});
+  const [editingCollection, setEditingCollection] = useState(false);
+
+  const [textValue, setTextValue] = useState<string>('');
+  const onTextChange = (e: any) => setTextValue(e.target.value);
+
   const { collectionId } = useParams();
   const dispatch = useDispatch();
   const collections = useSelector(
@@ -17,7 +24,10 @@ function CollectionDetail() {
       (collection) => collection.id === collectionId
     );
 
-    if (foundCollection) setCollectionDetailData(foundCollection);
+    if (foundCollection) {
+      setCollectionDetailData(foundCollection);
+      setTextValue(foundCollection.name);
+    }
   }
 
   function removeAnimeFromCollection(animeData) {
@@ -27,13 +37,35 @@ function CollectionDetail() {
     findCollectionDetail();
   }
 
+  function toggleEdit() {
+    setEditingCollection(!editingCollection);
+  }
+
+  function editCollectionName() {
+    dispatch(
+      editCollection({
+        collectionId,
+        name: textValue
+      })
+    );
+    setEditingCollection(false);
+  }
+
   useEffect(() => {
     findCollectionDetail();
   }, []);
   return (
     <>
       <h2>Collection Detail</h2>
-      {collectionDetailData?.name}
+      {editingCollection ? (
+        <TextField onChange={onTextChange} value={textValue} />
+      ) : (
+        textValue
+      )}
+
+      <Button onClick={editingCollection ? editCollectionName : toggleEdit}>
+        {editingCollection ? 'Submit' : 'Edit'}
+      </Button>
 
       <h3>Anime List</h3>
       {collectionDetailData &&
