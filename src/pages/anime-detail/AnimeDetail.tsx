edd4b971498da2211edb00b32f933acb39c6ab37
Aditya Leksono <adityaputra7997@gmail.com>
2023-07-31 07:15:45 +0700

    render collection list on anime detail page

--- src/pages/anime-detail/AnimeDetail.tsx
+++ src/pages/anime-detail/AnimeDetail.tsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -9,8 +9,27 @@ import { addAnime } from '../../store/animeCollections';
 
 function AnimeDetail() {
   const [animeDetail, setAnimeDetail] = useState({});
+  const [collectionListBelong, setCollectionListBelong] = useState([]);
   const { animeId } = useParams();
   const dispatch = useDispatch();
+
+  const collections = useSelector(
+    (state) => state.animeCollections.collections
+  );
+
+  function getCollectionBelonging() {
+    let collectionList: string[] = [];
+    collections.forEach((collection) => {
+      const foundAnimeList =
+        collection.animeList &&
+        collection.animeList.length > 0 &&
+        collection.animeList.find((animeItem) => animeItem.id == animeId);
+
+      if (foundAnimeList) collectionList.push(collection.name);
+    });
+
+    setCollectionListBelong(collectionList);
+  }
 
   const query = `
   query ($id: Int) { 
@@ -48,6 +67,7 @@ function AnimeDetail() {
   useEffect(() => {
     async function populateData() {
       await apiFetcher(query, { id: animeId }, handleData, handleError);
+      getCollectionBelonging();
     }
 
     populateData();
@@ -57,7 +77,6 @@ function AnimeDetail() {
     <>
       <h2>Anime Detail</h2>
       <img src={animeDetail.bannerImage} alt="" />
-
       <Typography gutterBottom variant="h1" component="div">
         {animeDetail.title?.english} ({animeDetail.title?.native})
       </Typography>
@@ -67,7 +86,8 @@ function AnimeDetail() {
       {animeDetail.season}
       <br />
       {animeDetail.genres?.toString()}
-
+      <br />
+      Collections : {collectionListBelong.toString()}
       <br />
       <Button onClick={addToCollection}>Add Anime To Collection</Button>
     </>
