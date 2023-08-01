3669423c39338e8e1f51055679652b1ea80bdfc9
Aditya Leksono <adityaputra7997@gmail.com>
2023-08-01 10:55:46 +0700

    add light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+
 import { router } from './routes/index.js';
 
 import { LoadingProvider } from './context/LoadingContext';
 import { SnackbarProvider } from './context/SnackbarContext';
 
+const theme = createTheme({
+  palette: {
+    mode: 'light'
+  }
+});
+
 function App() {
   return (
     <>
-      <LoadingProvider>
-        <SnackbarProvider>
-          <RouterProvider router={router} />
-        </SnackbarProvider>
-      </LoadingProvider>
+      <ThemeProvider theme={theme}>
+        <LoadingProvider>
+          <SnackbarProvider>
+            <RouterProvider router={router} />
+          </SnackbarProvider>
+        </LoadingProvider>
+      </ThemeProvider>
     </>
   );
 }
